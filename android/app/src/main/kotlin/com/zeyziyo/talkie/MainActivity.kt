5aa542b97e89ca1285fd817b5ba30bb0374144dc
사용자이름 <>
2026-01-17 03:34:05 +0900

    Fix volume control: Force STREAM_MUSIC in MainActivity

--- android/app/src/main/kotlin/com/zeyziyo/talkie/MainActivity.kt
+++ android/app/src/main/kotlin/com/zeyziyo/talkie/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.zeyziyo.talkie
 
+import android.media.AudioManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        volumeControlStream = AudioManager.STREAM_MUSIC
+    }
+}
